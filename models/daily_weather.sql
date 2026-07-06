a2d5fcc9d8ce3a9f62f27d31becd08f8bbaaf677
pavan kumar <pavandbtlabs@gmail.com>
2026-07-06 08:15:47 +0000

    daily weather model created

---
+++ models/daily_weather.sql
@@ -0,0 +1,33 @@
+with daily_weather as(
+
+SELECT 
+date(time) as daily_weather,
+weather,
+temp,
+pressure,
+humidity,
+clouds
+FROM {{source('demo','WEATHER')}}
+
+
+
+),
+ 
+ daily_weather_agg as(
+
+select 
+daily_weather,
+weather,
+round(avg(temp),2) as avg_temp,
+round(avg(pressure),2) as avg_pressure,
+round(avg(humidity),2) as avg_humidity,
+round(avg(clouds),2)as avg_clouds
+
+from daily_weather
+
+group by daily_weather,weather
+qualify row_number() over (partition by daily_weather order by count(weather) desc) =1
+
+ )
+
+SELECT * FROM daily_weather_agg
